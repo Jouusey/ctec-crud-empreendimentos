9960579539d487a347d025d48155eec0393fb0c2
jouusey <josevitor110m.p@gmail.com>
2026-03-14 11:10:20 -0300

    funcionalidade: criei os camps de nome e de responsavel junto com a funcao de mudança de estado usando o React

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,13 @@ function App() {
 
   })
 
+  const handleChange = (e) => {
+
+    const { name, value } = e.target
+
+    setFormData({ ...formData, [name]: value})
+  }
+
 
   return (
     <div className="p-8 bg-gray-100 min-h-screen font-sans text-gray-800">
@@ -22,14 +29,41 @@ function App() {
         SCTEC - Gestão de Empreendimentos SC
       </h1>
       
-     <div className="max-w-md mx-auto bg-white p-6 rounded shadow-md">
-        {/* Só para testar, vamos mostrar na tela o que tem no estado */}
-        <p className="text-sm text-gray-500 mb-4">
-          Testando o Estado:<br/>
-          Nome atual: <strong>{formData.nomeEmpreendimento}</strong>
-        </p>
+      <div className="max-w-md mx-auto bg-white p-6 rounded shadow-md">
         
-        {/* Aqui vamos colocar o input de verdade na próxima etapa */}
+        <div className="bg-yellow-50 p-2 text-sm text-yellow-800 mb-4 rounded border border-yellow-200">
+          <p>Memória em tempo real:</p>
+          <p>Nome: <strong>{formData.nomeEmpreendimento}</strong></p>
+          <p>Responsável: <strong>{formData.responsavel}</strong></p>
+        </div>
+
+        <form className="flex flex-col gap-4">
+          
+          <div>
+            <label className="block text-sm font-medium mb-1">Nome do Empreendimento *</label>
+            <input 
+              type="text" 
+              name="nomeEmpreendimento" 
+              value={formData.nomeEmpreendimento} 
+              onChange={handleChange} 
+              className="w-full border rounded p-2 focus:ring-2 focus:ring-blue-500 outline-none" 
+              placeholder="Ex: Tech SC" 
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">Empreendedor(a) Responsável *</label>
+            <input 
+              type="text" 
+              name="responsavel" 
+              value={formData.responsavel} 
+              onChange={handleChange} 
+              className="w-full border rounded p-2 focus:ring-2 focus:ring-blue-500 outline-none" 
+              placeholder="Ex: Maria Silva" 
+            />
+          </div>
+
+        </form>
       </div>
     </div>
   );
